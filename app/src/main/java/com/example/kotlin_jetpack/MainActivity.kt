78809e24c3502ae073f73238a27f2eb7a4a1a9b3
hemant <hemantkumar692@gmail.com>
2022-01-26 14:40:00 +0530

    Data Binding

---
+++ app/src/main/java/com/example/kotlin_jetpack/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.kotlin_jetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.kotlin_jetpack.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val quoteobj = Quotes("this is quotes","hemant kumar")
+        binding.mainv =quoteobj
+     //   binding.quoteNames.text = "hello hemant"
+//        binding.quoteTextAuthor.text = "THis is author"
+    }
+}
---
+++ app/src/main/java/com/example/kotlin_jetpack/Quotes.kt
@@ -0,0 +1,3 @@
+package com.example.kotlin_jetpack
+
+data class Quotes(val quote_name : String, val author: String)
